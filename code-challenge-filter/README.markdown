f9d7c630987ca1e947c7029a326c5a139b7771bb
karaCDev <72567414+karacdev@users.noreply.github.com>
2021-07-14 10:08:18 -0400

    Add files via upload

---
+++ code-challenge-filter/README.markdown
@@ -0,0 +1,5 @@
+# Code Challenge: Filter
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/karacdev/pen/VwbPqjx](https://codepen.io/karacdev/pen/VwbPqjx).
+
+
---
+++ code-challenge-filter/src/script.js
@@ -0,0 +1,24 @@
+const pets = ["goldfish", "dog", "turtle", "tiger"];
+
+//1 
+const petsShortNames = pets.filter( pets => pets.length <= 5);
+// The below line should console.log: ["dog", "tiger"]
+console.log(petsShortNames);
+
+//2 - uncomment line 9 and line 11 when you've finished challenge 1
+const petsTNames = 
+      pets.filter(pets => pets.includes('t'));
+// The below line should console.log: ["turtle", "tiger"] 
+ console.log(petsTNames);
+
+const numbers = [3, 7, 4, 5, 10];
+//3 - uncomment line 15 and line 17 
+const numbersBiggerThanFour = numbers.filter(n => n > 4);
+// The below line should console.log: [7, 5, 10]
+console.log(numbersBiggerThanFour);
+
+//Bonus - uncomment line 20 and line 22
+//const evenNumbers = 
+// The below line should console.log: [4, 10]
+//console.log(evenNumbers);
+
